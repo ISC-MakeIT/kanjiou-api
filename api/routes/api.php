82b16dd7aaa2419fd1a51ffdfeeec5d497a693e5
田代智 <r03.web.2110258@da.iwasaki.ac.jp>
2022-04-08 20:13:10 +0900

    :eyes:

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Register;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+    
 });
+
+Route::get('/', [Register::class, 'Index']);
+Route::get('/list', [Register::class, 'AllView']);
+Route::post('/DataPush', [Register::class, 'CatchData']);
+
+
