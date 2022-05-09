00bf742cbedfedc7f6d79fbcfe98914ff8dc548e
YahiroRyo <honakafn@gmail.com>
2022-05-09 20:21:48 +0900

    ランキング機能修正

--- api/app/Http/Controllers/RankController.php
+++ api/app/Http/Controllers/RankController.php
@@ -10,7 +10,7 @@ use App\Models\Rank;
 class RankController extends Controller
 {
     function ranked(RankedRequest $request) {
-        $rank = Rank::where("seconds", "<", $request->seconds)->count() + 1;
+        $rank = Rank::where("seconds", ">", $request->seconds)->count() + 1;
         $res = ["is_ranked" => $rank <= 100];
         if ($rank <= 100) {
             $res += ["rank" => $rank];
@@ -24,6 +24,6 @@ class RankController extends Controller
         return response(true, 200);
     }
     function ranks() {
-        return Rank::orderBy("seconds", "asc")->get();
+        return Rank::orderBy("seconds", "desc")->get();
     }
 }
